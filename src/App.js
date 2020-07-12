5db71e46837aac8af122b95dfb28ee4c7be2b7f3
soleme <soleme@gmail.com>
2020-07-12 14:49:43 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function BlackDog(){
+  this.name = '흰둥이';
+  return{
+    name:'검둥이',
+    bark:function(){
+      console.log(this.name + ': 멍멍!');
+    }
+  }
 }
+
+const blackDog = new BlackDog();
+blackDog.bark(); // 검둥이: 멍멍!
+
+function WhitDog(){
+  this.name = '흰둥이';
+  return{
+    name:'검둥이',
+    bark: () => {console.log(this.name + ': 멍멍!');}
+  }
+}
+
+const whiteDog = new WhitDog();
+whiteDog.bark(); // 흰둥이 : 멍멍!
+
+const App = () => {
+  const name = "리액트";
+
+  return <div className="react">{name}</div>;
+
+};
+
+// 일반 함수는 자신이 종속된 객체를 this로 가리키며, 화살표 함수는 자신이 종속된 인스턴스를 가리킨다.
 
 export default App;
